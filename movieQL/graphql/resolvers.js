b0300311e3cd122e1731b9f99795493acf79ff25
hanul-cha <ckgks3014@naver.com>
2021-11-25 20:31:37 +0900

    rest보다 간단하게 원하는것만 가져올수 있어서 좋은듯하다

--- movieQL/graphql/resolvers.js
+++ movieQL/graphql/resolvers.js
@@ -1,12 +1,30 @@
-const hanulCha = {
-    name: "hanul-cha",
-    age: 25,
-    gender: "male"
-}
+const people = [
+    {
+        name: "hanul-cha",    
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "sonmin-kim",    
+        age: 35,
+        gender: "male"
+    },
+    {
+        name: "inju-moon",    
+        age: 40,
+        gender: "male"
+    },
+    {
+        name: "ganghy-park",    
+        age: 20,
+        gender: "male"
+    },
+
+]
 
 const resolvers = {
     Query: {
-        person: () => hanulCha
+        people: () => people
     }
 };
 
