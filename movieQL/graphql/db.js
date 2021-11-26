e5e30d85691779bb2afbf1c73e407f9c4304a8ef
hanul-cha <ckgks3014@naver.com>
2021-11-26 18:21:01 +0900

    node-fetch보단 axios

--- movieQL/graphql/db.js
+++ movieQL/graphql/db.js
@@ -1,60 +1,9 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars",    
-        score: 20
-    },
-    {
-        id: 1,
-        name: "Avengers",    
-        score: 3
-    },
-    {
-        id: 2,
-        name: "About Time",    
-        score: 20
-    },
-    {
-        id: 3,
-        name: "Notting Hill",    
-        score: 40
-    },
-    {
-        id: 4,
-        name: "Freed",    
-        score: 80
-    },
-    {
-        id: 5,
-        name: "Dances Wolves",    
-        score: 26
-    },
-]
+import axios from "axios";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getMovies = () => movies
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id)
-    if(movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-}//일치하는 아이디에 맞는것만 빼고 새로운 배열 생성후 비교해서 movies에 재할당해줌
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length,
-        //+1을 빼야 되지 않나???
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    const test = axios.get(`${API_URL}`)
+    .then(res => res.data)
+    .then(json => json.data.movies);
+    return test
 }
