7766c1c6fe4bbd29d8d26c81a839653496f87eb0
hanul-cha <ckgks3014@naver.com>
2021-11-26 18:41:52 +0900

    특정조건으로 데이터 가져오기

--- movieQL/graphql/db.js
+++ movieQL/graphql/db.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-    const test = axios.get(`${API_URL}`)
+    let REQUEST_URL = API_URL;
+    if(limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
+    }
+    if(rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+
+    const movieList = axios.get(`${REQUEST_URL}`)
     .then(res => res.data)
     .then(json => json.data.movies);
-    return test
+    return movieList
 }
