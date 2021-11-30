779a28d840023fc55e4d4132b03e0df752c42ec3
hanul-cha <ckgks3014@naver.com>
2021-11-30 20:05:54 +0900

    제이쿼리로 해야되나...?

--- full-page/src/index.js
+++ full-page/src/index.js
@@ -3,4 +3,13 @@ import { cake } from './main'
 
 console.log(cake)
 
-console.log(Pageable)
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+    licenseKey: 'YOUR_KEY_HERE'
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
